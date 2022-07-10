4f307358420a40d0e3650f227e4778365364f753
Li-Chih Yen <98919376+lichih-yen@users.noreply.github.com>
2022-07-10 18:31:44 -0400

    Create 238. Product of Array Except Self.js
    Medium

---
+++ 238. Product of Array Except Self.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+/*
+IOC =>
+Input: nums
+Output: all the element except for itself
+Condition: O(n), without division
+*/
+
+// nums = [1, 2, 3, 4]
+// leftProduct = [1, 1, 2, 6]
+// rightProduct = [24, 12, 4, 1]
+// result = left * right => [24, 12, 8, 6]
+
+var productExceptSelf = function(nums) {
+    let leftProduct = [];
+    let rightProduct = [];
+    let result = [];
+    
+    // populate leftProduct
+    for (let i = 0; i < nums.length; i ++) {
+        if (leftProduct.length === 0){
+          leftProduct.push(1);  
+        } else {
+            leftProduct.push(leftProduct[i - 1] * nums[i - 1]);
+        }    
+    }
+    
+    // populate rightProduct
+    for (let i = nums.length - 1; i > -1; i --) {
+        if (rightProduct.length === 0) {
+            rightProduct.push(1);
+        } else {
+            rightProduct.unshift(rightProduct[0] * nums[i + 1])
+        }
+    }
+    
+    // populate result
+    for (let i = 0; i < leftProduct.length; i++) {
+        result.push(leftProduct[i] * rightProduct[i]);
+    }
+        
+    return result;
+};
