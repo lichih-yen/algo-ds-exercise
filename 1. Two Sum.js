b9063c5ac0a973601eafa46840833e48ffc1602f
Li-Chih Yen (Richie) <98919376+lichih-yen@users.noreply.github.com>
2022-07-17 21:30:09 -0400

    Update 1. Two Sum.js

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -1,5 +1,6 @@
-const nums = [1, 2, 3, 6, 7, 9];
-const target = 9;
+// const nums = [1, 2, 3, 6, 7, 9];
+// const target = 9;
+
 // This is way one:
 // function twoSum(nums, target) {
 //     for (let i = 0; i < nums.length; i++) {
@@ -15,14 +16,16 @@ const target = 9;
 
 // optimized way:
 function twoSum(nums, target) {
-    let memory = {};
+    const previousValue = {};
     for (let i = 0; i < nums.length; i++) {
-       if (memory[nums[i]] == undefined) {
-           memory[target - nums[i]] = i;
-       } else {
-            return console.log(memory[nums[i]], i)
-       }
+        const currentNumber = nums[i];
+        const needValue = target - currentNumber;
+        const index2 = previousValue[needValue];
+        if(index2 != null) {
+            return [index2, i]
+        } else {
+            previousValue[currentNumber] = i;
+        }
+        
     }
-}
 
-twoSum(nums, target);
